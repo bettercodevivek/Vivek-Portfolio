9a86558d8d228f21f997ec356a6c3e16da17ea51
bettercodevivek <viveksingh0520@gmail.com>
2024-10-10 18:05:38 +0530

    Updation

--- Portfolio/src/Components/Navbar.tsx
+++ Portfolio/src/Components/Navbar.tsx
@@ -3,7 +3,22 @@ import React from "react";
 const Navbar = () =>{
     return(
       <React.Fragment>
-
+       <nav className="flex flex-row items-center justify-between">
+        <img src="" alt="logo"/>
+        <div className="flex flex-row items-center justify-end gap-8">
+            <a href="#">About</a>
+            <a href="#">Experience</a>
+            <a href="#">Projects</a>
+            <a href="#">Blogs</a>
+            <a href="#">Contact</a>
+            <a href="#">Resume</a>
+        </div>
+        <div className="flex flex-col gap-1 mr-12 sm:hidden">
+         <div className="h-1 w-8 bg-black"></div>
+         <div className="h-1 w-7 bg-black"></div>
+         <div className="h-1 w-6 bg-black"></div>
+        </div>
+       </nav>
       </React.Fragment>
     );
 }
